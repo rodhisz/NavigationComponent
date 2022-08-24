30c9cc1ba4ba04e69284f5445b61d72268e427b4
rodhisz <rodhisz.45@gmail.com>
2022-08-24 22:30:04 +0700

    update

--- app/src/main/java/com/rsz/navigationcomponent/HomeFragment.kt
+++ app/src/main/java/com/rsz/navigationcomponent/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
 
@@ -13,7 +15,13 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val button = view.findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+        }
+        return view
     }
 
 }
--- app/src/main/java/com/rsz/navigationcomponent/ProfileFragment.kt
+++ app/src/main/java/com/rsz/navigationcomponent/ProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 
 class ProfileFragment : Fragment() {
@@ -14,7 +16,12 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val button = view.findViewById<Button>(R.id.button2)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_homeFragment)
+        }
+        return view
     }
 
 }
